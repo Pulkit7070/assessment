301ce730ba33cf8815fac90cce0fc61572c82c48
Pulkit Saraf <prateeksaraf9@gmail.com>
2025-08-27 11:09:46 +0530

    Add Render deployment configuration and fix server port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 // Enable CORS for all routes
 app.use(cors());
